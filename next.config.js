e40a99249fe536333b696e81ff257d908451071f
Alexander Hardinan <blackchef.alex@gmail.com>
2025-09-16 15:48:48 +0700

    Enable Next.js i18n config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true }
-module.exports = nextConfig;
+// next.config.js
+const { i18n } = require("./next-i18next.config");
+
+module.exports = {
+  i18n,
+  reactStrictMode: true,
+};
