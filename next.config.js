aa694c8a4ba6062329e1ef0bece63d0b24877ce2
Alexander Hardinan <blackchef.alex@gmail.com>
2026-07-16 22:38:57 -1000

    fix(seo): redirect Vercel domain to official domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 // next.config.js
 const { i18n } = require("./next-i18next.config");
 
+/** @type {import("next").NextConfig} */
 module.exports = {
   i18n,
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "black-and-white-website.vercel.app",
+          },
+        ],
+        destination: "https://tcwgazette.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
