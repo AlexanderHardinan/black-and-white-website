55f3cb280e1bf8d67852c9d3ed8be19e2ec4e8a3
Alexander Hardinan <blackchef.alex@gmail.com>
2026-07-16 23:16:02 -1000

    chore(seo): remove obsolete Vercel domain redirect

--- next.config.js
+++ next.config.js
@@ -5,20 +5,4 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   i18n,
   reactStrictMode: true,
-
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "black-and-white-website.vercel.app",
-          },
-        ],
-        destination: "https://tcwgazette.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
